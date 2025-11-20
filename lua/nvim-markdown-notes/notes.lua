43bb0a3491be49719d2a442a17c5312491b5e7ea
Emerick Bosch <xpc.dev@gmail.com>
2025-11-21 00:24:34 +0100

    Fix creating new note

--- lua/nvim-markdown-notes/notes.lua
+++ lua/nvim-markdown-notes/notes.lua
@@ -1,30 +1,30 @@
 local M = {}
 
 -- Create a new note with title input, formatted as "YYYY-MM-dd title.md"
-M.create_note = function()
-  assert(M.notes_root_path, "notes_root_path must be configured")
+M.create_note = function(notes_root_path)
+	assert(notes_root_path, "notes_root_path must be configured")
 
-  local title = vim.fn.input("Note title: ")
-  if title == "" then
-    print("Note creation cancelled")
-    return
-  end
+	local title = vim.fn.input("Note title: ")
+	if title == "" then
+		print("Note creation cancelled")
+		return
+	end
 
-  local today = vim.fn.strftime("%Y-%m-%d")
-  local note_filename = today .. " " .. title .. ".md"
-  local note_filepath = vim.fn.expand(vim.fn.resolve(M.notes_root_path .. "/" .. note_filename))
+	local today = vim.fn.strftime("%Y-%m-%d")
+	local note_filename = today .. " " .. title .. ".md"
+	local note_filepath = vim.fn.expand(vim.fn.resolve(notes_root_path .. "/" .. note_filename))
 
-  if type(note_filepath) == "string" then
-    if vim.fn.filereadable(note_filepath) == 1 then
-      print("Note already exists: " .. note_filename)
-      vim.cmd('e ' .. vim.fn.fnameescape(note_filepath))
-    else
-      local header = "# " .. title
-      os.execute('echo "' .. header .. '" > "' .. note_filepath .. '"')
-      vim.cmd('e ' .. vim.fn.fnameescape(note_filepath))
-      print("Created note: " .. note_filename)
-    end
-  end
+	if type(note_filepath) == "string" then
+		if vim.fn.filereadable(note_filepath) == 1 then
+			print("Note already exists: " .. note_filename)
+			vim.cmd("e " .. vim.fn.fnameescape(note_filepath))
+		else
+			local header = "# " .. title
+			os.execute('echo "' .. header .. '" > "' .. note_filepath .. '"')
+			vim.cmd("e " .. vim.fn.fnameescape(note_filepath))
+			print("Created note: " .. note_filename)
+		end
+	end
 end
 
 return M
