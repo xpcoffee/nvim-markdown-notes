e820c6856f54ddd60c4c434c40b6f7dc6d914181
Emerick Bosch <xpc.dev@gmail.com>
2026-03-10 09:04:46 +0100

    Fix journal day selection when file doesn't exist

--- lua/nvim-markdown-notes/journal.lua
+++ lua/nvim-markdown-notes/journal.lua
@@ -2,7 +2,7 @@ local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
-local conf = require("telescope.config").values
+local sorters = require("telescope.sorters")
 local notes = require("nvim-markdown-notes.notes")
 
 local M = {}
@@ -40,19 +40,19 @@ M.open_journal = function()
             }
           end,
         }),
-        sorter = conf.generic_sorter({}),
+        sorter = sorters.empty(),
         attach_mappings = function(prompt_bufnr, _)
           actions.select_default:replace(function()
             actions.close(prompt_bufnr)
             local selection = action_state.get_selected_entry()
             local journal_file_path = notes.get_file_path(M.opts.journal_dir_path, selection.value)
 
-            if type(journal_file_path) == "string" then
-              if vim.fn.filereadable(journal_file_path) == 1 then
-                vim.cmd("edit " .. journal_file_path)
-              else
-                os.execute('echo "# ' .. selection.value .. '" > ' .. journal_file_path)
-                vim.cmd("edit " .. journal_file_path)
+            if type(journal_file_path) == "string" and vim.fn.filereadable(journal_file_path) == 1 then
+              vim.cmd("edit " .. journal_file_path)
+            else
+              local file_path = notes.create_note(selection.value, M.opts.journal_dir_path, selection.value)
+              if file_path then
+                vim.cmd("edit " .. vim.fn.fnameescape(file_path))
               end
             end
           end)
